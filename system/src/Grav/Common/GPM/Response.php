f9f0029dee0b702537e105939a89611e350d9cbc
kittyfish <kittyfishfrommars@gmail.com>
2024-02-11 19:41:05 +0100

    feat(release): prepare file structure for release

--- system/src/Grav/Common/GPM/Response.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Create alias for the deprecated class.
-class_alias(\Grav\Common\HTTP\Response::class, \Grav\Common\GPM\Response::class);
--- user/plugins/form/scss.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sass --watch -s compressed scss:assets  
